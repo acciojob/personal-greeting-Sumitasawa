75a290ef3cc22f09cdee8a7afe72bca79661b689
Sumitasawa <sumitasawa65@gmail.com>
2025-11-03 15:32:21 +0530

    Initaal Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name,setName]=useState("");
   return (
     <div>
         {/* Do not remove the main div */}
+        <lable for="namee">Enter Your Name:</lable>
+        <input type="text" id="namee" value={name}
+          onChange={(e) => setName(e.target.value)}>
+          </input>
+
+          {name.trim() !== "" && (
+        <h3 style={{ marginTop: "20px" }}>Hello, {name}!</h3>
+      )}
     </div>
   )
 }
