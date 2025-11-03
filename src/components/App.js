6c8b14b982c70570106f093702b56bf06ad8b05c
Sumitasawa <sumitasawa65@gmail.com>
2025-11-03 15:35:37 +0530

    First

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,25 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
 
-const App = () => {
+const PersonalizedGreeting = () => {
+  const [name, setName] = useState("");
 
-  const [name,setName]=useState("");
   return (
-    <div>
-        {/* Do not remove the main div */}
-        <lable for="namee">Enter Your Name:</lable>
-        <input type="text" id="namee" value={name}
-          onChange={(e) => setName(e.target.value)}>
-          </input>
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h2>Personalized Greeting</h2>
 
-          {name.trim() !== "" && (
-        <h3 style={{ marginTop: "20px" }}>Hello, {name}!</h3>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ padding: "8px", width: "200px" }}
+      />
+
+      {name !== "" && (
+        <h3 style={{ marginTop: "20px" }}>Hello {name}!</h3>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default PersonalizedGreeting;
